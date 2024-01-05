ce39abcce26bfaea69b3dcfd4981dd4e40fde22b
lucocozz <lucocozz@student.42.fr>
2024-01-05 17:10:57 +0100

    add gdt

---
+++ kernel/cpu/segmentation/memory_segments.c
@@ -0,0 +1,32 @@
+#include "cpu/memory_segments.h"
+
+gdt_entry_t gdt_entries[SEGMENT_DESCRIPTOR_COUNT] = {0};
+
+void	gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran)
+{
+	gdt_entries[num].base_low = (base & 0xFFFF);
+	gdt_entries[num].base_middle = (base >> 16) & 0xFF;
+	gdt_entries[num].base_high = (base >> 24) & 0xFF;
+
+	gdt_entries[num].limit_low = (limit & 0xFFFF);
+	gdt_entries[num].granularity = gran;
+	gdt_entries[num].access = access;
+}
+
+void	init_gdt()
+{
+	gdt_ptr_t *gdt_ptr = GDT_ADDRESS;
+
+	gdt_ptr->limit = (sizeof(gdt_entry_t) * SEGMENT_DESCRIPTOR_COUNT);
+	gdt_ptr->base = (uint32_t)&gdt_entries;
+
+	gdt_set_gate(0, 0, 0, 0, 0);													// Null segment
+	gdt_set_gate(1, 0, SEGMENT_LIMIT, SEGMENT_KERNEL_CODE, SEGMENT_GRANULARITY);	// Kernel mode Code segment
+	gdt_set_gate(2, 0, SEGMENT_LIMIT, SEGMENT_KERNEL_DATA, SEGMENT_GRANULARITY);	// Kernel mode Data segment
+	gdt_set_gate(3, 0, SEGMENT_LIMIT, SEGMENT_KERNEL_STACK, SEGMENT_GRANULARITY);	// Kernel mode Stack segment
+	gdt_set_gate(4, 0, SEGMENT_LIMIT, SEGMENT_USER_CODE, SEGMENT_GRANULARITY);		// User mode code segment
+	gdt_set_gate(5, 0, SEGMENT_LIMIT, SEGMENT_USER_DATA, SEGMENT_GRANULARITY);		// User mode data segment
+	gdt_set_gate(6, 0, SEGMENT_LIMIT, SEGMENT_USER_STACK, SEGMENT_GRANULARITY);		// User mode stack segment
+
+	gdt_flush((uint32_t)&gdt_ptr);
+}

5f905e400ac3e9e7233cfc9d65424b52e607c900
lucocozz <lucocozz@student.42.fr>
2024-03-14 16:04:22 +0100

    added: save stack and clean register when panic

--- kernel/system/utils/panic.c
+++ kernel/system/utils/panic.c
@@ -1,8 +1,24 @@
 #include "system/utils.h"
 
+static void	__clean_registers(void)
+{
+	ASM(
+		"xor %%eax, %%eax;"
+		"xor %%ecx, %%ecx;"
+		"xor %%edx, %%edx;"
+		: : : "eax", "ecx", "edx"
+	);
+}
+
 void panic(const char *msg)
 {
+	stack_state_t *stack;
+
 	printk("KERNEL PANIC: %s\n", msg);
+	
+	ASM("mov %%esp, %0" : "=r"(stack));	// to save a trace of the stack at panic
+	__clean_registers();				// to avoid somes exceptions
+	
 	ASM("cli; hlt");
 }
 EXPORT_SYMBOL(panic);
